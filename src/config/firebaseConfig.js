fc18f1546ae73a4ee182d924ab6763e356b1bcaa
Gaurav <85581658+Gaurav4code@users.noreply.github.com>
2022-11-29 19:28:42 +0530

    Initial commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDMwuRKG5yN4gThtgkYOb_HbSPAlg0ua1U",
+  authDomain: "crypto-real.firebaseapp.com",
+  projectId: "crypto-real",
+  storageBucket: "crypto-real.appspot.com",
+  messagingSenderId: "818951926367",
+  appId: "1:818951926367:web:99542ec1619a6fb64821ea"
+};
+
+export default firebaseConfig;
